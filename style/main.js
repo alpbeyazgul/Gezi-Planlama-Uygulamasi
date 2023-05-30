1c4a5d28c3692cd73644dd246dc5f8f70df6897b
Alp Beyazgül <alpbeyazgul@hotmail.com>
2023-05-31 00:22:57 +0300

    Create main.js

---
+++ style/main.js
@@ -0,0 +1,8 @@
+// Ana JavaScript dosyası
+// Burada etkinlik dinleyicileri, Ajax istekleri veya diğer işlevler yer alabilir
+
+// Örneğin:
+// const button = document.querySelector('.btn');
+// button.addEventListener('click', function() {
+//     // İşlemler burada yapılır
+// });
